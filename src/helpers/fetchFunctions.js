15399e8671f88ecc96efa5c41f4ecc593d2bd96d
Aline Machado <alinedasmachado@gmail.com>
2023-04-20 13:50:44 -0300

    ex

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,9 +1,1 @@
-const heroImage = () => {
-    return fetch('https://superheroapi.com/api/9106948396042621/character-id/image')
-    .then((respondeu) => respondeu.json)
-}
 
-const heroName = () => {
-   return fetch('https://superheroapi.com/api/9106948396042621/search/name')
-    .then((respondeu) => respondeu.json)
-}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import { heroImage } from './helpers/fetchFunctions';
-import { heroName } from './helpers/fetchFunctions';
-
 const btn = document.querySelector('#button')
 const paragrathName = document.querySelector('#heroname')
 const image = document.querySelector('#heroimage')
+const idSuperHero = '9106948396042621';
+const URL  = `https://www.superheroapi.com/api.php/${idSuperHero}`;
+const numberHeros = 800;
+const ramdomHero = () => Math.floor(Math.random() * numberHeros);
+console.log(ramdomHero())
 
-btn.addEventListener('click', () => {
-   const response =  heroImage();
-   console.log(response);
+btn.addEventListener('click', (event) => {
+   event.preventDefault();
+   const numberHeroId = ramdomHero();
+   fetch(`${URL}/${numberHeroId}`)
+   .then((resposta) => resposta.json())
+   .then((baseDate) => {
+      image.src = baseDate.image.url;
+      paragrathName.innerHTML = baseDate.name;
+   })
+   .catch(error => console.error(error))
 })
